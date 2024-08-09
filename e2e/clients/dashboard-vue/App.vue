2c1887c75be488f288778e331010831520f3fdf6
OfeigrTenebris <OfeigrTenebris@yandex.ru>
2024-08-09 09:05:01 +0300

    Improving function of e2e/clients/dashboard-vue/App.vue

---
+++ e2e/clients/dashboard-vue/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <dashboard :Tool="Tool" />
+</template>
+
+<script>
+import Vue from 'vue'
+import Tool from '@Tool/core'
+import { Dashboard } from '@Tool/vue'
+
+export default {
+  name: 'App',
+  components: {
+    Dashboard,
+  },
+  computed: {
+    Tool: () => new Tool(),
+  },
+}
+</script>
+
+<style src="@Tool/core/dist/style.css"></style>
+<style src="@Tool/dashboard/dist/style.css"></style>
