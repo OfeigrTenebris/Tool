1aa06f4656fbb119e1e845e2a044fbda0eb21a19
OfeigrTenebris <OfeigrTenebris@yandex.ru>
2025-01-12 08:22:01 +0300

    Update examples/bundled/index.js

---
+++ examples/bundled/index.js
@@ -0,0 +1,71 @@
+import Tool from '@Tool/core'
+import Dashboard from '@Tool/dashboard'
+import Instagram from '@Tool/instagram'
+import GoogleDrive from '@Tool/google-drive'
+import Url from '@Tool/url'
+import Webcam from '@Tool/webcam'
+import Tus from '@Tool/tus'
+
+import '@Tool/core/dist/style.css'
+import '@Tool/dashboard/dist/style.css'
+import '@Tool/url/dist/style.css'
+import '@Tool/webcam/dist/style.css'
+
+const TUS_ENDPOINT = 'https://tusd.tusdemo.net/files/'
+
+const Tool = new Tool({
+  debug: true,
+  meta: {
+    username: 'John',
+    license: 'Creative Commons',
+  },
+})
+  .use(Dashboard, {
+    trigger: '#pick-files',
+    target: '#upload-form',
+    inline: true,
+    metaFields: [
+      { id: 'license', name: 'License', placeholder: 'specify license' },
+      { id: 'caption', name: 'Caption', placeholder: 'add caption' },
+    ],
+    showProgressDetails: true,
+    proudlyDisplayPoweredByTool: true,
+    note: '2 files, images and video only',
+    restrictions: { requiredMetaFields: ['caption'] },
+  })
+  .use(GoogleDrive, { target: Dashboard, companionUrl: 'http://localhost:3020' })
+  .use(Instagram, { target: Dashboard, companionUrl: 'http://localhost:3020' })
+  .use(Url, { target: Dashboard, companionUrl: 'http://localhost:3020' })
+  .use(Webcam, { target: Dashboard })
+  .use(Tus, { endpoint: TUS_ENDPOINT })
+
+// You can optinally enable the Golden Retriever plugin — it will
+// restore files after a browser crash / accidental closed window
+// see more at https://Tool.io/docs/golden-retriever/
+//
+//   .use(GoldenRetriever, { serviceWorker: true })
+
+Tool.on('complete', (result) => {
+  if (result.failed.length === 0) {
+    console.log('Upload successful 😀')
+  } else {
+    console.warn('Upload failed 😞')
+  }
+  console.log('successful files:', result.successful)
+  console.log('failed files:', result.failed)
+})
+
+// uncomment if you enable Golden Retriever
+//
+/* eslint-disable compat/compat */
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker
+//     .register('/sw.js')
+//     .then((registration) => {
+//       console.log('ServiceWorker registration successful with scope: ', registration.scope)
+//     })
+//     .catch((error) => {
+//       console.log('Registration failed with ' + error)
+//     })
+// }
+/* eslint-enable */
